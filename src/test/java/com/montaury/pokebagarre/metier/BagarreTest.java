f1c70597694c377d6360f805d07684f6d2a7018d
Juan David Rodriguez <rs2003jd@gmail.com>
2024-04-02 15:29:39 +0200

    Fin TP4 Mokito

--- src/test/java/com/montaury/pokebagarre/metier/BagarreTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/BagarreTest.java
@@ -6,19 +6,26 @@ import com.montaury.pokebagarre.erreurs.ErreurRecuperationPokemon;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
+import com.montaury.pokebagarre.webapi.PokeBuildApi;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
-
 class BagarreTest {
-
     Bagarre  bagarre;
+    PokeBuildApi fausseApi;
+
     @BeforeEach
     void creer_bagarre(){
-        this.bagarre = new Bagarre();
+        fausseApi = Mockito.mock(PokeBuildApi.class);
+                this.bagarre = new Bagarre(fausseApi);
     }
 
     @Test
@@ -67,5 +74,106 @@ class BagarreTest {
         assertThat(erreur)
                 .isInstanceOf(ErreurMemePokemon.class)
                 .hasMessage("Impossible de faire se bagarrer un pokemon avec lui-meme");
+    }
+
+    @Test
+    void devrait_envoyer_erreur_quand_premier_pokemon_non_rensaigne(){//given
+        Mockito.when(fausseApi.recupererParNom("pikachu"))
+                .thenReturn(CompletableFuture.failedFuture(new ErreurRecuperationPokemon("pikachu"))
+                );
+
+        Mockito.when(fausseApi.recupererParNom("mewtwo"))
+                .thenReturn(CompletableFuture.completedFuture(new Pokemon("mewtwo", "url2",
+                                new Stats(3, 24))
+                        )
+                );
+        //when
+        CompletableFuture<Pokemon> futurVainqueur = bagarre.demarrer ("pikachu","mewtwo");
+
+        //then
+        assertThat(futurVainqueur)
+                .failsWithin(Duration.ofSeconds(2))
+                .withThrowableOfType(ExecutionException.class)
+                .havingCause()
+                .isInstanceOf(ErreurRecuperationPokemon.class)
+                .withMessage("Impossible de recuperer les details sur 'pikachu'") ;
+    }
+
+    @Test
+    void devrait_envoyer_erreur_quand_second_pokemon_non_rensaigne(){
+        //given
+        Mockito.when(fausseApi.recupererParNom("mewtwo"))
+                .thenReturn(CompletableFuture.completedFuture(new Pokemon("mewtwo", "url1",
+                                new Stats(3, 24))
+                        )
+                );
+        Mockito.when(fausseApi.recupererParNom("pikachu"))
+                .thenReturn(CompletableFuture.failedFuture(new ErreurRecuperationPokemon("pikachu"))
+                );
+        //when
+        CompletableFuture<Pokemon> futurVainqueur = bagarre.demarrer ("mewtwo","pikachu");
+
+        //then
+        assertThat(futurVainqueur)
+                .failsWithin(Duration.ofSeconds(2))
+                .withThrowableOfType(ExecutionException.class)
+                .havingCause()
+                .isInstanceOf(ErreurRecuperationPokemon.class)
+                .withMessage("Impossible de recuperer les details sur 'pikachu'") ;
+    }
+
+
+    @Test
+    void devrait_envoyer_victoir_du_second_car_plus_attaque() {
+        //given
+        Mockito.when(fausseApi.recupererParNom("mewtwo"))
+                .thenReturn(CompletableFuture.completedFuture(new Pokemon("mewtwo", "url1",
+                                new Stats(3, 24))
+                        )
+                );
+        Mockito.when(fausseApi.recupererParNom("mew"))
+                .thenReturn(CompletableFuture.completedFuture(new Pokemon("mew", "url2",
+                                new Stats(30, 24))
+                        )
+                );
+        //when
+        CompletableFuture<Pokemon> futurVainqueur = bagarre.demarrer("mewtwo", "mew");
+
+        //then
+        assertThat(futurVainqueur)
+                .succeedsWithin(Duration.ofSeconds(2))
+                .satisfies(pokemon -> {
+                            assertThat(pokemon.getNom())
+                                    .isEqualTo("mew");
+                        }
+                );
+
+    }
+
+    @Test
+    void devrait_envoyer_victoir_du_second_car_egalite() {
+        //given
+        Mockito.when(fausseApi.recupererParNom("mewtwo"))
+                .thenReturn(CompletableFuture.completedFuture(new Pokemon("mewtwo", "url1",
+                                new Stats(30, 24))
+                        )
+                );
+        Mockito.when(fausseApi.recupererParNom("mew"))
+                .thenReturn(CompletableFuture.completedFuture(new Pokemon("mew", "url2",
+                                new Stats(30, 24))
+                        )
+                );
+        //when
+        CompletableFuture<Pokemon> futurVainqueur = bagarre.demarrer("mewtwo", "mew");
+
+        //then
+        assertThat(futurVainqueur)
+                .succeedsWithin(Duration.ofSeconds(2))
+                .satisfies(pokemon -> {
+                            assertThat(pokemon.getNom())
+                                    .isEqualTo("mewtwo");
+                        }
+                );
+
     }
 }
